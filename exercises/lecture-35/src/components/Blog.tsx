1ad132e2183d23c4c88f6263166448480f40d6a9
Kateryna <kateryna.ksk@gmail.com>
2024-04-18 19:05:50 +0200

    tasks for lecture 35

---
+++ exercises/lecture-35/src/components/Blog.tsx
@@ -0,0 +1,19 @@
+function Blog(props: any) {
+  const { post } = props;
+
+  return (
+    <article className="post">
+      <div className="cover-container">
+        <img src={post.cover} alt={post.title} />
+      </div>
+      <div className="post-footer">
+        <h3>
+          {post.title} {post.id}
+        </h3>
+        <p>{post.content}</p>
+      </div>
+    </article>
+  );
+}
+
+export default Blog;
